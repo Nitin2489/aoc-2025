ad2b6d242ac8b2bf80a803ce1bc851be48831392
shayd3 <ryan.ross215@gmail.com>
2025-12-02 21:53:08 -0500

    Day 2 init

--- days/day02/solution.py
+++ days/day02/solution.py
@@ -1,6 +1,18 @@
+'''
+The ranges are separated by commas (,); each range gives its first ID and last ID separated by a dash (-).
+
+find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+
+None of the numbers have leading zeroes; 0101 isn't an ID at all. (101 is a valid ID that you would ignore.)
+
+Your job is to find all of the invalid IDs that appear in the given ranges.
+Add up all of the invalid IDs and return value
+'''
+
 def part1(data: str) -> int:
     lines = data.strip().split('\n')
     # Your solution here
+    print(lines)
     return 0
 
 def part2(data: str) -> int:
