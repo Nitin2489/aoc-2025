859d5a17c8019ab1f1ab04259ea86560ec832754
shayd3 <ryan.ross215@gmail.com>
2025-12-08 16:30:29 -0500

    Day 3 Part 1: Init

--- days/day03/solution.py
+++ days/day03/solution.py
@@ -1,6 +1,23 @@
+'''
+Input: Each line of digits in input corresponds to a single bank of batteries
+
+We need to turn on exactly 2 batteries within a bank of batteries.
+
+Need to find the largest possible joltage each bank can produce (note: you can NOT rearrange batteries)
+
+examples:
+987654321111111 -> largest possible is 98
+811111111111119 -> 89 is largest
+234234234234278 -> 78 is the largest
+818181911112111 -> 92 is the largest
+
+return sum of the maximum joltage from each bank
+'''
+
 def part1(data: str) -> int:
     lines = data.strip().split('\n')
-    # Your solution here
+    for line in lines:
+        print(line)
     return 0
 
 def part2(data: str) -> int:
