b1d7783dd30f9a3c9cc172fc3e69131a5d2bce76
shayd3 <ryan.ross215@gmail.com>
2025-12-16 21:43:20 -0500

    Add utiltiies.parse class

---
+++ utilities/parse.py
@@ -0,0 +1,17 @@
+class Parse():
+
+    @staticmethod
+    def str_to_list(input: str, delimiter: str = '\n') -> list:
+        """Parse the input on a given delimiter.
+
+        Args:
+            input (str): The input string.
+            delimiter (str): The delimiter to split the input on. Defaults to '\n'.
+
+        Returns:
+            list: The input string split on the delimiter."""
+        return [line for line in input.split(delimiter)]
+
+    @staticmethod
+    def to_2d_list(input: str) -> list:
+        return [list(line) for line in input.splitlines()]
